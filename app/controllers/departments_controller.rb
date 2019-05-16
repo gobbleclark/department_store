23893a028f5b7879d6074d150cd4efa9c633f16e
Gavin Clark <gavingclark4@gmail.com>
2019-05-15 21:55:40 -0600

    child controler and model working

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,55 @@
 class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show, :edit, :update, :destroy]
   def index
+    @departments = Department.all
   end
 
   def show
+    #@department = Department.find(params[:id])
   end
 
   def new
+    @department = Department.new
+    render partial: "form"
   end
 
   def edit
+    #@department = Department.find(params[:id])
+      render partial: "form"
   end
+
+  def create
+     @department = Department.new(department_params)
+      if @department.save
+      redirect_to departments_path
+      else render partial: "form"
+      end
+
+  end
+
+  def update
+    #@department = Department.find(params[:id])
+    if @department.update(department_params)
+      redirect_to departments_path(@department.id)
+    else 
+      render partial: "form"
+    end 
+  end
+
+  def destroy
+    #@department = Department.find(params[:id])
+    @department.destroy
+    redirect_to departments_path
+
+  end 
+  
+  private
+  def department_params
+    #params = {departments: { name: "Department Name"}}
+    params.require(:department).permit(:name)
+   end 
+ 
+   def set_department
+    @department = Department.find(params[:id])
+   end
 end
